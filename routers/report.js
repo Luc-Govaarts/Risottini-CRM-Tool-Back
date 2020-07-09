d4c6e75ec5df382c90b71f61b969a2aa197bbd26
Luc-Govaarts <lucgovaarts@hotmail.com>
2020-07-09 16:09:46 +0200

    first deployable version

--- routers/report.js
+++ routers/report.js
@@ -18,10 +18,8 @@ router.get("/:id", async (req, res, next) => {
 })
 
 router.post("/", async (req, res, next) => {
-    const { name, leadId, note } = req.body
+    const { userId, leadId, note } = req.body
     try {
-        const user = await User.findOne({where: {name}, include: User})
-        const userId = user.dataValues.id
         const newReport = await Report.create({userId, leadId, note})
         return res.status(200).send(newReport)
     } catch(error) {
