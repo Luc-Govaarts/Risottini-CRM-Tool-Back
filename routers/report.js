76d3fc0f83773ffdcba5128d7995d68665f147ca
Luc-Govaarts <lucgovaarts@hotmail.com>
2020-07-28 13:54:47 +0200

    Added patch report route

--- routers/report.js
+++ routers/report.js
@@ -4,18 +4,18 @@ const { Router } = require("express");
 
 const router = new Router();
 
-router.get("/:id", async (req, res, next) => {
-    const leadId = parseInt(req.params.id)
-    console.log("LEAD ID B:", leadId)
-    try {
-        const reports = await Report.findAll({where: {leadId}})
-        return res.
-            status(200).send(reports)    
-    } catch(error) {
-        console.log(error)
-        return res.status(400).send(`something went wrong`)
-    }
-})
+// router.get("/:id", async (req, res, next) => {
+//     const leadId = parseInt(req.params.id)
+//     console.log("LEAD ID B:", leadId)
+//     try {
+//         const reports = await Report.findAll({where: {leadId}})
+//         return res.
+//             status(200).send(reports)    
+//     } catch(error) {
+//         console.log(error)
+//         return res.status(400).send(`something went wrong`)
+//     }
+// })
 
 router.post("/", async (req, res, next) => {
     const { userId, leadId, note } = req.body
@@ -23,6 +23,28 @@ router.post("/", async (req, res, next) => {
         const newReport = await Report.create({userId, leadId, note})
         return res.status(200).send(newReport)
     } catch(error) {
+        console.log(error)
+        return res.status(400).send(`something went wrong`)
+    }
+})
+
+router.patch("/:id", async (req, res, next) => {
+    const reportId = parseInt(req.params.id)
+    const { id, note, userId, leadId } = req.body
+
+    try {
+        const reportToUpdate = await Report.findByPk(id)
+        if(!reportToUpdate) {
+            return res.
+            status(404).send(`No report found with id ${id}`)
+        } else {
+            await reportToUpdate.update({note, userId, leadId})
+            const updatedReport = await Report.findByPk(id)
+            return res.
+            status(200).
+            send(updatedReport)
+        }
+    } catch {
         console.log(error)
         return res.status(400).send(`something went wrong`)
     }
