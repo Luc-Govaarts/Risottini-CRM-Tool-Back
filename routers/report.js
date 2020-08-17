dd90e8ca3ed9967a7c685fa5f6ec0f3e96ba98bb
Luc-Govaarts <lucgovaarts@hotmail.com>
2020-08-17 16:14:54 +0200

    Added get al reports route

--- routers/report.js
+++ routers/report.js
@@ -4,18 +4,16 @@ const { Router } = require("express");
 
 const router = new Router();
 
-// router.get("/:id", async (req, res, next) => {
-//     const leadId = parseInt(req.params.id)
-//     console.log("LEAD ID B:", leadId)
-//     try {
-//         const reports = await Report.findAll({where: {leadId}})
-//         return res.
-//             status(200).send(reports)    
-//     } catch(error) {
-//         console.log(error)
-//         return res.status(400).send(`something went wrong`)
-//     }
-// })
+router.get("/", async (req, res, next) => {
+    try {
+        const reports = await Report.findAll()
+        return res.
+            status(200).send(reports)    
+    } catch(error) {
+        console.log(error)
+        return res.status(400).send(`something went wrong`)
+    }
+})
 
 router.post("/", async (req, res, next) => {
     const { userId, leadId, note } = req.body
